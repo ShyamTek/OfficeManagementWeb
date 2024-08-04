9e73e4a76d5e4d943714fb59a42e0afffb4c95e5
KALPESH\Kalpesh <kalpeshborse1432@gmail.com>
2024-08-04 15:38:58 +0530

    show department name

--- Tawuniya.Web/Tawuniya.Web/Tawuniya.Web/Models/Employees/EmployeeListModel.cs
+++ Tawuniya.Web/Tawuniya.Web/Tawuniya.Web/Models/Employees/EmployeeListModel.cs
@@ -8,7 +8,7 @@
         public string Email { get; set; }
         public string Number { get; set; }
         public string IpAddress { get; set; }
-        public int DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
         public DateTime BookingStartDate { get; set; }
         public DateTime BookingEndDate { get; set; }
         public DateTime CreatedOn { get; set; }
